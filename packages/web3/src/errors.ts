0d12ba39150abef850d47094a963eebfe7b75a61
ross-weir <29697678+ross-weir@users.noreply.github.com>
2024-10-17 19:30:17 +1100

    add configs

--- packages/web3/src/errors.ts
+++ packages/web3/src/errors.ts
@@ -1,14 +1,8 @@
 export class SovereignError extends Error {
   constructor(message: string) {
     super(message);
-    this.name = this.constructor.name;
 
-    // `Error.captureStackTrace` is v8 specific API,
-    // add fallback for compatibility in other envs
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, this.constructor);
-    } else {
-      this.stack = new Error().stack;
-    }
+    this.name = this.constructor.name;
+    this.stack = new Error().stack;
   }
 }
