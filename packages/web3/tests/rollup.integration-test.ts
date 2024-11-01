72a5134d58e9a914fd3a80d5f409643b8ba4b445
ross-weir <29697678+ross-weir@users.noreply.github.com>
2024-11-01 22:31:55 +1100

    remove integration test in web3

--- packages/web3/tests/rollup.integration-test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it } from "vitest";
-
-describe("rollup", () => {
-  describe("submit transaction", () => {
-    it("should successfully submit transactions", () => {});
-  });
-});
