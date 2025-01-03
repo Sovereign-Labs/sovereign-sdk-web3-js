515279e5ee83c0f424ae594abe534e8714cec5e4
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-01-03 18:02:19 +1100

    add wasm plugin for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vitest/config";
+import wasm from "vite-plugin-wasm";
 
 function includePackages(...packages: string[]) {
   return packages.map((p) => `packages/${p}/**/*.ts`);
 }
 
 export default defineConfig({
+  plugins: [wasm()],
   test: {
     coverage: {
       provider: "v8",
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,13 +1,16 @@
 import { defineWorkspace } from "vitest/config";
+import wasm from "vite-plugin-wasm";
 
 export default defineWorkspace([
   {
+    plugins: [wasm()],
     test: {
       name: "unit",
       include: ["**/*.test.ts"],
     },
   },
   {
+    plugins: [wasm()],
     test: {
       name: "integration",
       include: ["**/*.integration-test.ts"],
