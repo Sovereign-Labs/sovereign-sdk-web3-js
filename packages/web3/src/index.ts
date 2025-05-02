137d52d4c36e1692fee3c242386c351bba827cf1
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-05-02 19:39:03 +1000

    add util to get addresses from pub key

--- packages/web3/src/index.ts
+++ packages/web3/src/index.ts
@@ -5,3 +5,4 @@ export * from "./type-spec";
 export { type EventPayload, Subscription } from "./subscriptions";
 export * as SovereignClient from "@sovereign-sdk/client";
 export { Schema } from "@sovereign-sdk/universal-wallet-wasm";
+export { HexString } from "./utils";
