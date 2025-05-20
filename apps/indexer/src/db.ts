be1d4d2dcb5f53c96efdee76edd655af6025df28
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-05-20 22:58:59 +1000

    move to indexing via polling sequencer api

--- apps/indexer/src/db.ts
+++ apps/indexer/src/db.ts
@@ -10,11 +10,8 @@ export type EventSchema = {
 
 export type Database<T> = {
   get inner(): T;
-  getMissingEvents: (
-    limit?: number,
-    latestEventNumber?: number,
-  ) => Promise<number[]>;
-  insertEvent: (event: EventSchema) => Promise<void>;
+  getLatestEventNumber(): Promise<number | null>;
+  insertEvents: (events: EventSchema[]) => Promise<void>;
   disconnect: () => Promise<void>;
 };
 
@@ -25,45 +22,38 @@ export function postgresDatabase(connectionString: string): PostgresDatabase {
 
   return {
     inner: pool,
-    async getMissingEvents(limit?: number, latestEventNumber?: number) {
-      const values = [limit ?? 500];
-
-      if (latestEventNumber) {
-        values.push(latestEventNumber);
+    async getLatestEventNumber(): Promise<number | null> {
+      const result = await pool.query("SELECT MAX(number) FROM rollup_events;");
+      return result.rows[0].max;
+    },
+    async insertEvents(events) {
+      if (!events || events.length === 0) {
+        return;
       }
 
-      // This query will likely be slow when there's lots of events in the db
-      // can re-visit if this turns out to be the case
-      const result = await pool.query(
-        `
-        WITH max_value AS (
-          SELECT COALESCE(${latestEventNumber ? "$2::integer" : "NULL"}, 
-                          (SELECT max(number) FROM rollup_events)) AS max_number
-        ),
-        all_numbers AS (
-          SELECT generate_series(0, (SELECT max_number FROM max_value)) AS number
-        )
-        SELECT a.number 
-        FROM all_numbers a
-        LEFT JOIN rollup_events e ON a.number = e.number
-        WHERE e.number IS NULL
-        ORDER BY a.number
-        LIMIT $1;
-      `,
-        values,
-      );
+      const placeholders = events
+        .map((_, index) => {
+          const offset = index * 4;
+          return `($${offset + 1}, $${offset + 2}, $${offset + 3}, $${
+            offset + 4
+          })`;
+        })
+        .join(", ");
 
-      return result.rows.map((row) => row.number);
-    },
-    async insertEvent(event) {
       const query = `
         INSERT INTO rollup_events 
           (number, key, value, module) 
         VALUES 
-          ($1, $2, $3, $4)
+          ${placeholders}
         RETURNING id;
       `;
-      const values = [event.number, event.key, event.value, event.module];
+
+      const values = events.flatMap((event) => [
+        event.number,
+        event.key,
+        event.value,
+        event.module,
+      ]);
 
       await pool.query(query, values);
     },
--- apps/indexer/src/indexer.ts
+++ apps/indexer/src/indexer.ts
@@ -1,13 +1,12 @@
 import { type Rollup, SovereignClient } from "@sovereign-sdk/web3";
 import type { Database, EventSchema } from "./db";
 import logger from "./logger";
-import { chunkArray } from "./utils";
 
 export type IndexerOpts = {
   database: Database<unknown>;
   // biome-ignore lint/suspicious/noExplicitAny: types arent used
   rollup: Rollup<any, any>;
-  backfillIntervalMs?: number;
+  pollIntervalMs?: number;
   healthcheckIntervalMs?: number;
 };
 
@@ -15,14 +14,14 @@ export class Indexer {
   private readonly database: Database<unknown>;
   // biome-ignore lint/suspicious/noExplicitAny: types arent used
   private readonly rollup: Rollup<any, any>;
-  private readonly backfillIntervalMs: number;
+  private readonly pollIntervalMs: number;
   private readonly healthcheckIntervalMs: number;
-  private backfillHandle?: ReturnType<typeof setTimeout>;
+  private indexingHandle?: ReturnType<typeof setTimeout>;
   private healthcheckHandle?: ReturnType<typeof setTimeout>;
   private isRollupHealthy = true;
 
   constructor(opts: IndexerOpts) {
-    this.backfillIntervalMs = opts.backfillIntervalMs ?? 200;
+    this.pollIntervalMs = opts.pollIntervalMs ?? 100;
     this.healthcheckIntervalMs = opts.healthcheckIntervalMs ?? 5000;
     this.database = opts.database;
     this.rollup = opts.rollup;
@@ -38,16 +37,15 @@ export class Indexer {
       await this.handleRollupOffline();
     }
 
-    this.doBackfill();
-
-    logger.info("Subscribed to rollup events and performing event backfill");
+    logger.info("Rollup is healthy, indexing beginning");
+    this.doIndexing();
   }
 
   async stop(): Promise<void> {
     logger.info("Indexer is shutting down");
 
     logger.info("Clearing background tasks");
-    clearTimeout(this.backfillHandle);
+    clearTimeout(this.indexingHandle);
     clearTimeout(this.healthcheckHandle);
 
     logger.info("Disconnecting from database");
@@ -60,92 +58,43 @@ export class Indexer {
     logger.error("An error occurred", err);
   }
 
-  private async onNewEvent(event: EventSchema): Promise<void> {
-    logger.debug("Handling new event", event);
-    return this.database.insertEvent(event).catch((e) => this.onError(e));
-  }
-
-  private async doBackfill(): Promise<void> {
+  private async doIndexing(): Promise<void> {
     if (!this.isRollupHealthy) {
       // blocks until rollup is back online
       await this.handleRollupOffline();
     }
 
-    const missingEventNums = await this.getMissingEventNumbers();
-    logger.debug(
-      "Amount of events being backfilled",
-      missingEventNums.length ?? 0,
-    );
-    const events = await this.fetchEvents(missingEventNums);
+    const currentEventNum = (await this.database.getLatestEventNumber()) ?? 0;
+    logger.debug("Current index event number", currentEventNum);
 
-    // TODO: batch insert
-    for (const event of events) {
-      this.onNewEvent(event);
-    }
+    const events = await this.fetchEvents(currentEventNum);
 
-    this.backfillHandle = setTimeout(
-      () => this.doBackfill(),
-      this.backfillIntervalMs,
+    logger.debug(`Insert ${events.length} events`);
+    await this.database.insertEvents(events).catch((e) => this.onError(e));
+
+    this.indexingHandle = setTimeout(
+      () => this.doIndexing(),
+      this.pollIntervalMs
     );
   }
 
-  private async fetchEvents(eventNums: number[]): Promise<EventSchema[]> {
-    const chunkedEventNums = chunkArray(eventNums, 15);
-    const events: EventSchema[] = [];
-
-    // chunks can be pushed to workers if there's ever performance issues
-    for (const chunk of chunkedEventNums) {
-      const promises = chunk.map(async (n) => {
-        const eventResponse = await this.rollup.ledger.events.retrieve(n);
-
-        if (!eventResponse.data) {
-          throw new Error(
-            "Response didnt contain data field, this shouldn't be possible",
-          );
-        }
-
-        const event = eventResponse.data;
-        return { ...event, module: event.module.name };
-      });
-
-      for (const result of await Promise.allSettled(promises)) {
-        if (result.status === "fulfilled") {
-          events.push(result.value);
-        } else if (!this.setAndCheckHealth(result.reason)) {
-          logger.info("Rollup unreachable while fetching events");
-        }
-      }
-
-      // dont try any more chunks, they'll likely fail
-      if (!this.isRollupHealthy) {
-        return events;
-      }
-    }
-
-    return events;
-  }
-
-  private async getMissingEventNumbers(): Promise<number[]> {
+  private async fetchEvents(currentEventNum: number): Promise<EventSchema[]> {
     try {
-      const response = await this.rollup.ledger.events.latest();
-      const event = response.data;
-
-      return this.database.getMissingEvents(500, event?.number);
-    } catch (e) {
-      if (!this.setAndCheckHealth(e)) {
-        return [];
-      }
-
-      logger.debug(
-        "Failed to retrieve latest event, falling back to latest event number in db",
-      );
-      return this.database.getMissingEvents();
+      const response = await this.rollup.sequencer.events.list({
+        page: "next",
+        "page[cursor]": String(currentEventNum),
+        "page[size]": 50,
+      });
+      return response.data.items;
+    } catch (err) {
+      this.setAndCheckHealth(err);
+      return [];
     }
   }
 
   private handleRollupOffline(): Promise<void> {
     logger.info(
-      "Rollup appears to be offline, entering monitoring mode until it's healthy",
+      "Rollup appears to be offline, entering monitoring mode until it's healthy"
     );
 
     return new Promise((resolve, reject) => {
@@ -160,7 +109,7 @@ export class Indexer {
           }
           this.healthcheckHandle = setTimeout(
             doHealthcheck,
-            this.healthcheckIntervalMs,
+            this.healthcheckIntervalMs
           );
         } catch (err) {
           console.error("Error occurred during health check", err);
