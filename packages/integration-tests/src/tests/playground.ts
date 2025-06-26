fe36e3a08302a0cf01df1ffd625ef2b642a6a139
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-06-26 16:52:01 +1000

    repro rest api inconsistency

--- packages/integration-tests/src/tests/playground.ts
+++ packages/integration-tests/src/tests/playground.ts
@@ -3,12 +3,68 @@
 import {
   createStandardRollup,
   addressFromPublicKey,
+  StandardRollup,
 } from "@sovereign-sdk/web3";
 import { getSigner } from "./signer.js";
+import pRetry from "p-retry";
+
+let successes = 0;
+
+async function getState(
+  rollup: StandardRollup<{}>,
+  height: number
+): Promise<any> {
+  return pRetry(
+    async () => {
+      const result = await rollup.http.get(
+        "/modules/attester-incentives/state/rollup-finality-period",
+        {
+          query: {
+            slot_number: height,
+          },
+        }
+      );
+      successes += 1;
+
+      return result;
+    },
+    {
+      retries: 5,
+      factor: 2,
+      minTimeout: 1000,
+      maxTimeout: 10000,
+    }
+  );
+}
+
+async function slotNumberRepro(rollup: StandardRollup<{}>) {
+  const response = await rollup.ledger.slots.finalized();
+  const slotNumber = response.data!.number;
+
+  try {
+    const result = getState(rollup, slotNumber);
+    // const result = await rollup.http.get(
+    //   "/modules/attester-incentives/state/rollup-finality-period",
+    //   {
+    //     query: {
+    //       slot_number: slotNumber,
+    //     },
+    //     maxRetries: 200,
+    //   }
+    // );
+    successes += 1;
+    console.log("success with height", slotNumber);
+    setTimeout(() => slotNumberRepro(rollup), 0);
+  } catch (e) {
+    console.log("got a error", e);
+    console.log("succeed times", successes);
+  }
+}
 
 let subscription;
 const run = async () => {
   const rollup = await createStandardRollup({
+    url: "https://rollup.staging.bullet.xyz",
     context: {
       defaultTxDetails: {
         max_priority_fee_bips: 0,
@@ -19,14 +75,16 @@ const run = async () => {
     },
   });
 
-  const signer = getSigner(rollup.chainHash);
-  const publicKey = await signer.publicKey();
-  console.log(addressFromPublicKey(publicKey, "sov"));
+  slotNumberRepro(rollup as any);
 
-  console.log("subscribing to rollup events");
-  subscription = rollup.subscribe("events", async (event) => {
-    console.log(event);
-  });
+  // const signer = getSigner(rollup.chainHash);
+  // const publicKey = await signer.publicKey();
+  // console.log(addressFromPublicKey(publicKey, "sov"));
+
+  // console.log("subscribing to rollup events");
+  // subscription = rollup.subscribe("events", async (event) => {
+  //   console.log(event);
+  // });
 };
 
 await run();
