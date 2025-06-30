0ddd4cd8086dc2a53e37366859fe8877f355bf82
Nikolai Golub <nikolai@sovlabs.io>
2025-06-30 14:09:26 +0200

    proper message

--- packages/integration-tests/src/tests/rollup.integration-test.ts
+++ packages/integration-tests/src/tests/rollup.integration-test.ts
@@ -1,101 +1,113 @@
-import {describe, it, expect, beforeAll} from "vitest";
-import {createStandardRollup, StandardRollup} from "@sovereign-sdk/web3";
-import {getSigner} from "./signer";
-import {Signer} from "@sovereign-sdk/signers";
+import { describe, it, expect, beforeAll } from "vitest";
+import { createStandardRollup, StandardRollup } from "@sovereign-sdk/web3";
+import { getSigner } from "./signer";
+import { Signer } from "@sovereign-sdk/signers";
 
 let signer: Signer;
 let rollup: StandardRollup<any>;
 
+const testAddress = {
+  Standard: "sov1lzkjgdaz08su3yevqu6ceywufl35se9f33kztu5cu2spja5hyyf",
+};
+
 describe("rollup", async () => {
-    describe.sequential("transaction submission", () => {
-        it("should successfully sign and submit a transaction", async () => {
-            rollup = await createStandardRollup({
-                context: {
-                    defaultTxDetails: {
-                        max_priority_fee_bips: 0,
-                        max_fee: "100000000",
-                        gas_limit: null,
-                        chain_id: 4321,
-                    },
-                },
-            });
-            signer = getSigner(rollup.chainHash);
-            const runtimeCall = {
-                bank: {
-                    create_token: {
-                        token_name: "token_1",
-                        initial_balance: "20000",
-                        mint_to_address: signer,
-                        admins: [signer]
-                    }
-                }
-            };
-            const {response} = await rollup.call(runtimeCall, {
-                signer,
-            });
-            expect(response.data!.status).toEqual("submitted");
-        });
-        it("should submit a batch with manually incrementing nonces successfully", async () => {
-            rollup = await createStandardRollup({
-                context: {
-                    defaultTxDetails: {
-                        max_priority_fee_bips: 0,
-                        max_fee: "100000000",
-                        gas_limit: null,
-                        chain_id: 4321,
-                    },
-                },
-            });
-            signer = getSigner(rollup.chainHash);
-            const publicKey = await signer.publicKey();
-            let {nonce} = await rollup.dedup(publicKey);
-            const startingNonce = nonce;
-            const batch = [];
-            const callMessages = [
-                {
-                    bank: {
-                        create_token: {
-                            token_name: "token_1",
-                            initial_balance: "20000",
-                            mint_to_address: signer,
-                            admins: [signer]
-                        }
-                    }
-                },
-                {
-                    bank: {
-                        create_token: {
-                            token_name: "token_2",
-                            initial_balance: "20000",
-                            mint_to_address: signer,
-                            admins: [signer]
-                        }
-                    }
-                },
-                {
-                    bank: {
-                        create_token: {
-                            token_name: "token_3",
-                            initial_balance: "30000",
-                            mint_to_address: signer,
-                            admins: [signer]
-                        }
-                    }
-                },
-            ];
-
-            for (const callMessage of callMessages) {
-                const {transaction} = await rollup.call(callMessage, {
-                    signer,
-                    overrides: {generation: nonce},
-                });
-
-                batch.push(transaction);
-                nonce += 1;
-            }
-
-            expect(batch.length).toEqual(3);
-            expect(nonce).toEqual(startingNonce + 3);
-        });
+  describe.sequential("transaction submission", () => {
+    it("should successfully sign and submit a transaction", async () => {
+      rollup = await createStandardRollup({
+        context: {
+          defaultTxDetails: {
+            max_priority_fee_bips: 0,
+            max_fee: "100000000",
+            gas_limit: null,
+            chain_id: 4321,
+          },
+        },
+      });
+      signer = getSigner(rollup.chainHash);
+      const runtimeCall = {
+        bank: {
+          create_token: {
+            token_name: "token_1",
+            initial_balance: "20000",
+            token_decimals: 12,
+            supply_cap: "100000000000",
+            mint_to_address: testAddress,
+            admins: [testAddress],
+          },
+        },
+      };
+      const { response } = await rollup.call(runtimeCall, {
+        signer,
+      });
+      expect(response.data!.status).toEqual("submitted");
     });
+    it("should submit a batch with manually incrementing nonces successfully", async () => {
+      rollup = await createStandardRollup({
+        context: {
+          defaultTxDetails: {
+            max_priority_fee_bips: 0,
+            max_fee: "100000000",
+            gas_limit: null,
+            chain_id: 4321,
+          },
+        },
+      });
+      signer = getSigner(rollup.chainHash);
+      const publicKey = await signer.publicKey();
+      let { nonce } = await rollup.dedup(publicKey);
+      const startingNonce = nonce;
+      const batch = [];
+      const callMessages = [
+        {
+          bank: {
+            create_token: {
+              token_name: "token_1",
+              initial_balance: "20000",
+              token_decimals: 12,
+              supply_cap: "100000000000",
+              mint_to_address: testAddress,
+              admins: [testAddress],
+            },
+          },
+        },
+        {
+          bank: {
+            create_token: {
+              token_name: "token_2",
+              initial_balance: "20000",
+              token_decimals: 12,
+              supply_cap: "100000000000",
+              mint_to_address: testAddress,
+              admins: [testAddress],
+            },
+          },
+        },
+        {
+          bank: {
+            create_token: {
+              token_name: "token_3",
+              initial_balance: "30000",
+              token_decimals: 12,
+              supply_cap: "100000000000",
+              mint_to_address: testAddress,
+              admins: [testAddress],
+            },
+          },
+        },
+      ];
+
+      for (const callMessage of callMessages) {
+        const { transaction } = await rollup.call(callMessage, {
+          signer,
+          overrides: { generation: nonce },
+        });
+
+        batch.push(transaction);
+        nonce += 1;
+      }
+
+      expect(batch.length).toEqual(3);
+      expect(nonce).toEqual(startingNonce + 3);
+    });
+  });
 });
