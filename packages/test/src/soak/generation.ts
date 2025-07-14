2e7a0c872aae3f07f071649a6be1dd24119c4a17
Ross Weir <29697678+ross-weir@users.noreply.github.com>
2025-07-14 22:15:57 +1000

    Bump js client lib version (#173)
    * bump js client lib version
    
    * changeset

--- packages/test/src/soak/generation.ts
+++ packages/test/src/soak/generation.ts
@@ -1,10 +1,10 @@
-import type SovereignSDK from "@sovereign-sdk/client";
 import type { Signer } from "@sovereign-sdk/signers";
-import type { BaseTypeSpec } from "@sovereign-sdk/web3";
+import type { BaseTypeSpec, SovereignClient } from "@sovereign-sdk/web3";
 import { NotImplementedError } from "./errors";
 
 /** Response data type from a transaction submission */
-export type TxResult = SovereignSDK.Sequencer.TxCreateResponse.Data;
+export type TxResult =
+  SovereignClient.SovereignSDK.Sequencer.TxCreateResponse.Data;
 
 /**
  * Possible outcomes for a generated transaction.
