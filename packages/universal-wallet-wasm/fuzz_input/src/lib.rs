15db13944889e590a10a8341afeaf71c6dd86f03
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-07-28 21:12:47 +1000

    wip

--- packages/universal-wallet-wasm/fuzz_input/src/lib.rs
+++ packages/universal-wallet-wasm/fuzz_input/src/lib.rs
@@ -1,19 +1,41 @@
+use arbitrary::{Arbitrary, Unstructured};
 use sov_modules_api::macros::UniversalWallet;
 use sov_modules_api::prelude::serde::{Deserialize, Serialize};
 use sov_modules_api::SafeString;
+use sov_universal_wallet::schema::safe_string::DEFAULT_MAX_STRING_LENGTH;
 use sov_universal_wallet::schema::Schema;
 use wasm_bindgen::prelude::*;
 
+// arbitrary isn't implemented for safe string
 #[derive(Serialize, Deserialize, UniversalWallet)]
-pub enum StringInput {
-    Hex(SafeString),
-    Base58(SafeString),
-    Any(SafeString),
+struct ArbitrarySafeString(SafeString);
+
+impl Arbitrary<'_> for ArbitrarySafeString {
+    fn arbitrary(u: &mut Unstructured<'_>) -> arbitrary::Result<Self> {
+        let len = u.int_in_range(0..=DEFAULT_MAX_STRING_LENGTH)?;
+        let s = String::arbitrary(u)?;
+
+        let truncated = if s.len() > DEFAULT_MAX_STRING_LENGTH {
+            s.chars().take(len).collect()
+        } else {
+            s
+        };
+
+        Ok(ArbitrarySafeString(truncated.try_into().unwrap()))
+    }
 }
 
-#[derive(Serialize, Deserialize, UniversalWallet)]
-pub enum FuzzInput {
-    String(StringInput),
+#[derive(Serialize, Deserialize, UniversalWallet, Arbitrary)]
+enum ByteInput {
+    Hex(#[sov_wallet(display(hex))] Vec<u8>),
+    Base58(#[sov_wallet(display(base58))] Vec<u8>),
+    Decimal(#[sov_wallet(display(decimal))] Vec<u8>),
+}
+
+#[derive(Serialize, Deserialize, UniversalWallet, Arbitrary)]
+enum FuzzInput {
+    StringInput(ArbitrarySafeString),
+    ByteInput(ByteInput),
 }
 
 #[wasm_bindgen]
@@ -23,6 +45,16 @@ pub fn schema() -> Result<JsValue, JsValue> {
 }
 
 #[wasm_bindgen]
-pub fn generate_input() -> JsValue {
-    todo!()
+pub fn generate_input() -> Result<JsValue, JsValue> {
+    let mut data = vec![0u8; 128];
+    for byte in data.iter_mut() {
+        *byte = (js_sys::Math::random() * 256.0) as u8;
+    }
+
+    let mut u = Unstructured::new(&data);
+    let input = FuzzInput::arbitrary(&mut u)
+        .map_err(|e| JsValue::from_str(&format!("Failed to generate input: {:?}", e)))?;
+
+    serde_wasm_bindgen::to_value(&input)
+        .map_err(|e| JsValue::from_str(&format!("Failed to serialize: {:?}", e)))
 }
