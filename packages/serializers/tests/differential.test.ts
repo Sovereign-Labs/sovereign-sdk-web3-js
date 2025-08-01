e97264b8605bb375a9d5a8ded44d279a3aa2fc9f
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-08-01 18:46:50 +1000

    remove outdated comment

--- packages/serializers/tests/differential.test.ts
+++ packages/serializers/tests/differential.test.ts
@@ -4,9 +4,6 @@ import { JsSerializer } from "../src";
 import { WasmSerializer } from "../src/wasm";
 import demoRollupVectors from "./vectors/demo-rollup";
 
-// These tests run against the WASM implementation which requires JSON serialization.
-// JSON can introduce differences with certain types like f64s due to precision loss associated with serialization/deserialization,
-// so types that include such numbers can be unreliable to test here.
 describe("differential", () => {
   describe("demo rollup", () => {
     it.each(demoRollupVectors.calls.map((call, index) => ({ index, call })))(
