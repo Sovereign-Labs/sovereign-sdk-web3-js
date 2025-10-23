ad64ecbf28388ca59386807fc2bd7498d4652438
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-10-23 22:38:58 +1100

    add 'types' package to store core sovereign primitives

---
+++ packages/types/src/index.ts
@@ -0,0 +1,36 @@
+import type { HexString } from "@sovereign-sdk/utils";
+
+export type TxDetails = {
+  max_priority_fee_bips: number;
+  max_fee: string;
+  gas_limit: number[] | null;
+  chain_id: number;
+};
+
+export type Generation = { generation: number };
+export type Nonce = { nonce: number };
+export type Uniqueness = Nonce | Generation;
+
+export type UnsignedTransaction<RuntimeCall> = {
+  runtime_call: RuntimeCall;
+  uniqueness: Uniqueness;
+  details: TxDetails;
+};
+
+export type TransactionV0<RuntimeCall> = {
+  V0: {
+    pub_key: HexString;
+    signature: HexString;
+  } & UnsignedTransaction<RuntimeCall>;
+};
+
+// multie sig
+export type TransactionV1<RuntimeCall> = {
+  V1: {
+    unused_pub_keys: HexString[];
+    signatures: HexString[];
+    min_signers: number;
+  } & UnsignedTransaction<RuntimeCall>;
+};
+
+export type Transaction<RuntimeCall> = TransactionV0<RuntimeCall>;
